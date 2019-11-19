8c80d2ca55406d28f4ed11bb4dac8a5de3822439
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2019-11-18 21:06:30 -0500

    Debugged and Ready for Grading

--- MyStack.cpp
+++ MyStack.cpp
@@ -10,7 +10,7 @@ int main()
 {
     cout<<"\nLet's start working on stack: ";
     cout<<"\n0. Exit\n1. Stack(implemented based on array)\n2. Stack(implemented based on linked list)\n";
-    cout<<"Please enter choice: ";
+    cout<<"Please enter choice:";
     int n;
     cin>>n;
     switch (n)
